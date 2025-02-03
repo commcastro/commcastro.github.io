3eceae64205a34d91fb556634ce6c1beab59d9ca
commcastro <n.castro.ie@gmail.com>
2025-02-03 11:17:52 +0000

    Deploying to gh-pages from @ commcastro/commcastro.github.io@704062ff38c45e2f2626d6c1f3da22503132099a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -481,25 +481,25 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/nelson-castro-hn", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0003-3672-5161", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Bkcv-VAAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
